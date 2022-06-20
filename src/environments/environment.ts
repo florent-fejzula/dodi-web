9fefa16c81d5304aa43e9d880fbc197ff6362cae
Dodi <florent.fejzula@ved.mk>
2022-06-20 13:16:35 +0200

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'dodi-web',
+    appId: '1:1076938329862:web:7d06305259a5dd68901887',
+    storageBucket: 'dodi-web.appspot.com',
+    apiKey: 'AIzaSyAULsf1HNe343slQUA97G8OSaP5jjQD11I',
+    authDomain: 'dodi-web.firebaseapp.com',
+    messagingSenderId: '1076938329862',
+  },
   production: false
 };
 
